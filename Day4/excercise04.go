4eb9cb774b0be0ac41e20c058dfcd9e15feb8527
zhangqiang <sweetsmelon@163.com>
2018-03-30 18:31:18 +0800

    day04

--- Day4/excercise04.go
+++ Day4/excercise04.go
@@ -1,18 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	//1、打印100以内所有偶数
-	//func1()
-	//2、打印1-100以内，能被3整除，不能被5整除的数，每行打印5个
-	//func2()
-	//3、求1-100内，奇数的和
-	func3()
-	//4、打印2-100内的素数
-	func4()
-	//5、百元白鸡 一百元买100只鸡，公鸡5元/只，母鸡3元/只 小鸡1元/3只
-	func5()
+	////1、打印100以内所有偶数
+	////func1()
+	////2、打印1-100以内，能被3整除，不能被5整除的数，每行打印5个
+	////func2()
+	////3、求1-100内，奇数的和
+	//func3()
+	////4、打印2-100内的素数
+	//func4()
+	////5、百元白鸡 一百元买100只鸡，公鸡5元/只，母鸡3元/只 小鸡1元/3只
+	//func5()
+	////6、猜数游戏
+	////func6()
+	/*
+	     *				4	5
+		***				5	6
+	   *****			6	7
+	  *******			7	8
+	 *********				9
+	*/
+	n := 4
+	for i := 0; i < n; i++ {
+		for j := 0; j < n+i; j++ {
+			if j < n-i-1 {
+				fmt.Print(" ")
+			}else {
+				fmt.Print("*")
+			}
+		}
+		fmt.Println()
+	}
+}
+func func6()  {
+	rand.Seed(time.Now().Unix())
+	num1 := rand.Intn(100)+1
+	for  {
+		fmt.Println("猜")
+		temp := 0
+		fmt.Scanln(&temp)
+		if temp>num1 {
+			fmt.Println("大了")
+		}else if temp < num1 {
+			fmt.Println("小了")
+		}else {
+			fmt.Println("对喽。。。")
+			break
+		}
+	}
 }
 func func5() {
 	//5g+3m+x/3 = 100 元
@@ -25,11 +66,11 @@ func func5() {
 	moneyM := 0
 	totalX := 0
 	moneyX := 0
-	for totalG = 1; totalG <= 20; totalG++ {
+	for totalG = 0; totalG <= 20; totalG++ {
 		moneyG = 5 * totalG
-		for totalM = 1; totalM <= 33; totalM++ {
+		for totalM = 0; totalM <= 33; totalM++ {
 			moneyM = 3 * totalM
-			for totalX = 1; totalX <= total-totalG-totalM; totalX++ {
+			for totalX = 0; totalX <= total-totalG-totalM; totalX++ {
 				if totalX%3 == 0 {
 					moneyX = totalX / 3
 					if moneyX+moneyM+moneyG == total && totalX+totalM+totalG == total {
@@ -49,6 +90,7 @@ func func4() {
 			//fmt.Println(j,"%",i,j%i)
 			if i%j == 0 {
 				count++
+				break
 			}
 		}
 		if count == 1 {
